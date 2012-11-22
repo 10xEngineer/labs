c649edbaa27f786dc1401192730056ad93c7e6c6
Radim Marek <radim@laststation.net>
2012-11-22 13:34:05 +0100

    Allow optional name

--- lib/labs/utils/name.rb
+++ lib/labs/utils/name.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-def get_machine_name(args)
+def get_machine_name(args, optional = false)
 		name = args.shift
 		unless name
 			name = ENV["LAB_MACHINE"]
@@ -8,7 +8,7 @@ def get_machine_name(args)
 			say "debug: using lab machine '#{name}' from environment variable LAB_MACHINE\n\n" if name
 		end
 
-		abort('Machine name required') unless name
+		abort('Machine name required') unless name || optional
 
 		return name
 end
